5e43c7c2e7b46f3f77a746d98cdf916a5acf370c
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 17:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+
+function lowerCaseDrivers(drivers){
+  
+  return drivers.map(function(s){
+     return s.toLowerCase();
+  })
+  
+}
+
+
+function nameToAttributes(drivers){
+  
+  return drivers.map(function(s){
+      let temp = s.indexOf(s);
+      let str1 = s.slice(0, temp-1);
+      let str2 = s.slice(temp+1, s.length);
+      
+  });
+  
+}
